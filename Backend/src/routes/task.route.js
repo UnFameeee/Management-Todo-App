673f43d4fbb5a0f877158c2753b4b55dced28010
Quốc Thắng <quocthang036@gmail.com>
2022-07-03 16:22:37 +0700

    finshing config

--- Backend/src/routes/task.route.js
+++ Backend/src/routes/task.route.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const { addTask, updateTaskOwner } = require('../controller/task.controller')
+const { addTask, updateTaskOwner, updateData, viewTask } = require('../controller/task.controller')
 
 router.route('/create').post(addTask);
-router.route('/update/:id/user').put(updateTaskOwner)
+router.route('/update/:id/user').put(updateTaskOwner);
+router.route('/update/:id').put(updateData);
+router.route('/view/:id').get(viewTask);
 
 module.exports = router;
