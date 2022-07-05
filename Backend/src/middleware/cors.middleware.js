801275f48cac86f75c95fc8444ccbd5d4f6a4575
Quốc Thắng <quocthang036@gmail.com>
2022-07-05 14:05:16 +0700

    commit
    commit

--- Backend/src/middleware/cors.middleware.js
+++
@@ -1,8 +0,0 @@
-const cors = (req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Method', 'GET, POST, PUT, DELETE, PATCH');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    next();
-}
-
-module.exports = cors
